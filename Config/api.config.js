3d7048050804ed992234589327906feebb1cfc55
Gabriel Barreto <barreto-gabriel@outlook.com>
2018-09-04 22:57:36 -0300

    Add new prop for API Configs

--- Config/api.config.js
+++ Config/api.config.js
@@ -1,4 +1,8 @@
+const mode = process.env.NODE_ENV || "development";
+const port = process.env.PORT || 5000;
+
 module.exports = Object.freeze({
-    port: process.env.PORT || 5000,
-    mode: process.env.NODE_ENV || "development"
+    port,
+    mode,
+    isDev: mode == "development"
 });
