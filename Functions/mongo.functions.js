b0a7fe384b966cc68cdaa2eaa2746bd07a321cfd
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-17 03:17:23 -0300

    Add more config to mongo connection, return connection

--- Functions/mongo.functions.js
+++ Functions/mongo.functions.js
@@ -10,33 +10,37 @@ import config from '../Config/mongo.config';
  * @param { String } [forcedURI=null] forcing mongoose connect in that URI, ignoring config values. If null or empty mongoose assumes config value in current node running mode [development|production]
  */
 const connect = (forcedURI = null) => {
-    let uri = config;
-    if (forcedURI) {
-        uri = forcedURI;
-    }
+  let uri = config;
+  if (forcedURI) {
+    uri = forcedURI;
+  }
 
-    // ==> Create connection listeners
-    mongoose.connection.on('connected', () => {
-        console.log('[MONGO] Connected');
-    });
-    mongoose.connection.on('disconnected', () => {
-        console.warn('[MONGO] Disconnected');
-    });
-    mongoose.connection.on('error', err => {
-        console.error(`[MONGO] An error occurred: ${err.message}`);
-    });
-    mongoose.connection.on('open', () => {
-        console.log('[MONGO] Ready');
-    });
+  // ==> Create connection listeners
+  mongoose.connection.on('connected', () => {
+    console.log('[MONGO] Connected');
+  });
+  mongoose.connection.on('disconnected', () => {
+    console.warn('[MONGO] Disconnected');
+  });
+  mongoose.connection.on('error', err => {
+    console.error(`[MONGO] An error occurred: ${err.message}`);
+  });
+  mongoose.connection.on('open', () => {
+    console.log('[MONGO] Ready');
+  });
 
-    // Connecting
-    mongoose.connect(uri, {
-        keepAlive: true,
-        useNewUrlParser: true,
-    });
-    mongoose.Promise = global.Promise;
+  // Connecting
+  const connection = mongoose.connect(uri, {
+    keepAlive: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  mongoose.Promise = global.Promise;
+
+  return connection;
 };
 
 export default {
-    connect,
+  connect,
 };
