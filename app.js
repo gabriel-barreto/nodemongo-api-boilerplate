c18755e768cbc7e57bb4e66d6732ef3258d804b8
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-17 02:57:41 -0300

    Update config import

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 import logger from 'morgan';
 
 // ==> Getting configs
-import config from './Config/api.config';
+import { app as config } from './Config';
 
 // ==> Getting modules
 import Mongo from './Functions/mongo.functions';
@@ -24,34 +24,36 @@ app.use(logger(config.isDev ? 'dev' : 'tiny'));
 // ==> Injecting routers
 app.use('/api', Routers);
 
-// ==> Starting server
-const server = app.listen(config.port, err => {
-    if (err) throw err;
+const setup = err => {
+  if (err) throw err;
 
-    // ==> Showing API running infos
-    console.log('==> Services');
-    console.log('[API] ...ONLINE!');
-    console.log('\n==> Env');
-    console.log(`[API] Mode: ${config.mode}`);
-    console.log(`[API] Port: ${config.port}`);
-    console.log('\n==> URLs');
-    console.log(
-        `[API] Entry: ${
-            server.address().address == '::'
-                ? 'http://127.0.0.1:'
-                : server.address().address
-        }:${config.port}`,
-    );
-    console.log(
-        `[API] Check: ${
-            server.address().address == '::'
-                ? 'http://127.0.0.1:'
-                : server.address().address
-        }:${config.port}/api/run`,
-    );
+  // ==> Showing API running infos
+  console.log('==> Services');
+  console.log('[API] ...ONLINE!');
+  console.log('\n==> Env');
+  console.log(`[API] Mode: ${config.mode}`);
+  console.log(`[API] Port: ${config.port}`);
+  console.log('\n==> URLs');
+  console.log(
+    `[API] Entry: ${
+      server.address().address == '::'
+        ? 'http://127.0.0.1:'
+        : server.address().address
+    }:${config.port}`,
+  );
+  console.log(
+    `[API] Check: ${
+      server.address().address == '::'
+        ? 'http://127.0.0.1:'
+        : server.address().address
+    }:${config.port}/api/run`,
+  );
 
-    // ==> Connecting on Database
-    // NOTE: If you want to force another URI pass them as a parameter to connect function bellow
-    console.log('\n==> Database');
-    Mongo.connect();
-});
+  // ==> Connecting on Database
+  // NOTE: If you want to force another URI pass them as a parameter to connect function bellow
+  console.log('\n==> Database');
+  Mongo.connect();
+};
+
+export const Setup = setup;
+export default app;
