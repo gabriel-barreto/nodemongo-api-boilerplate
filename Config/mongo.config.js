e2b354713efaff2385f22ecbc5029d0b5c0b0d7e
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-17 02:59:06 -0300

    Update base config import

--- Config/mongo.config.js
+++ Config/mongo.config.js
@@ -2,10 +2,10 @@
 // ALERT: Garantee this pattern below
 // mongodb://[user]:[password]@[host]:[port]/[database]
 const config = Object.freeze({
-    development: 'mongodb://127.0.0.1:27017/nodemongo-api-boilerplate',
-    production: 'mongodb://127.0.0.1:27018/nodemongo-api-boilerplate',
+  development: 'mongodb://127.0.0.1:27017/nodemongo-api-boilerplate',
+  production: 'mongodb://127.0.0.1:27018/nodemongo-api-boilerplate',
 });
 
-import ApiConfig from './api.config';
+import AppConfig from './app.config';
 
-export default config[ApiConfig.mode];
+export default config[AppConfig.mode];
