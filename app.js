fa4ac76a11a4f27016d310ad5d41ffaded3f06b4
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-10-17 03:16:39 -0300

    Convert to use class style

--- app.js
+++ app.js
@@ -6,54 +6,50 @@ import helmet from 'helmet';
 import logger from 'morgan';
 
 // ==> Getting configs
-import { app as config } from './Config';
+import { app as Config } from './Config';
 
 // ==> Getting modules
 import Mongo from './Functions/mongo.functions';
 import Routers from './Routers';
 
-// ==> Create new express application
-const app = express();
+class App {
+  constructor() {
+    this.app = express();
 
-// ==> Inject dependencies
-app.use(bodyParser.json());
-app.use(helmet());
-app.use(compression());
-app.use(logger(config.isDev ? 'dev' : 'tiny'));
+    this.database();
+  }
 
-// ==> Injecting routers
-app.use('/api', Routers);
+  setup(err) {
+    if (err) throw err;
 
-const setup = err => {
-  if (err) throw err;
+    // ==> Showing API running infos
+    console.log('==> Services');
+    console.log('[API] ...ONLINE!');
+    console.log('\n==> Env');
+    console.log(`[API] Mode: ${Config.mode}`);
+    console.log(`[API] Port: ${Config.port}`);
+    console.log('');
+  }
 
-  // ==> Showing API running infos
-  console.log('==> Services');
-  console.log('[API] ...ONLINE!');
-  console.log('\n==> Env');
-  console.log(`[API] Mode: ${config.mode}`);
-  console.log(`[API] Port: ${config.port}`);
-  console.log('\n==> URLs');
-  console.log(
-    `[API] Entry: ${
-      server.address().address == '::'
-        ? 'http://127.0.0.1:'
-        : server.address().address
-    }:${config.port}`,
-  );
-  console.log(
-    `[API] Check: ${
-      server.address().address == '::'
-        ? 'http://127.0.0.1:'
-        : server.address().address
-    }:${config.port}/api/run`,
-  );
+  listen(port) {
+    this.server = this.app.listen(port, this.setup);
+    return this.server;
+  }
 
-  // ==> Connecting on Database
-  // NOTE: If you want to force another URI pass them as a parameter to connect function bellow
-  console.log('\n==> Database');
-  Mongo.connect();
-};
+  database() {
+    Mongo.connect();
+  }
 
-export const Setup = setup;
-export default app;
+  init() {
+    // ==> Inject dependencies
+    app.use(bodyParser.json());
+    app.use(helmet());
+    app.use(compression());
+    app.use(logger(config.isDev ? 'dev' : 'tiny'));
+
+    // ==> Injecting routers
+    app.use('/api', Routers);
+  }
+}
+
+export default App;
