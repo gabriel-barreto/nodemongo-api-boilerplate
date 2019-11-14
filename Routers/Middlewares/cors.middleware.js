fd9e2fe29613be6d38b4deaf725d7a08d009f722
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-11-14 05:01:22 -0300

    Fix allowed methods

--- Routers/Middlewares/cors.middleware.js
+++ Routers/Middlewares/cors.middleware.js
@@ -1,10 +1,7 @@
 // CORS/Default Middleware
 export default (_, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type');
-    res.header(
-        'Access-Control-Allow-Methods',
-        'DELETE, GET, POST, PUT, OPTIONS',
-    );
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type');
+  res.header('Access-Control-Allow-Methods', 'DELETE, GET, OPTIONS, POST, PUT');
+  next();
 };
