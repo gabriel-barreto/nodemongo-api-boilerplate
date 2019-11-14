36c36d731eb0db3f21cec416ec5c4fbc778e792b
Gabriel Barreto <barreto-gabriel@outlook.com>
2019-11-14 05:00:09 -0300

    Remove old files

--- Factory/example2.factory.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-/* DO SOMETHIN GREAT!
-
-    PUT YOUR CODE HERE!
-
-*/
--- Queries/example.queries.js
+++
@@ -1,9 +0,0 @@
-// Import model
-// import model from "../Models/[model_name].model";
-// or
-// import { model } from "../Models";
-
-// Exporting your model queries
-// export default {
-// list: model.find({}),
-// };
